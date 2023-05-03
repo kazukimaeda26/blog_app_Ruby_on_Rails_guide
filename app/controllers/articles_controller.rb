14640585d118b932cf61c21bcbf71ce96fc8c412
Kazuki Maeda <maedakazuki26@gmail.com>
2023-05-03 23:08:27 +0900

    コメント機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,8 @@ class ArticlesController < ApplicationController
   end
 
   private
-    def article_params
-      params.require(:article).permit(:title, :body)
-    end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_134209) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_230_502_234_052) do
+  create_table 'articles', force: :cascade do |t|
+    t.string 'title'
+    t.text 'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.string 'body'
+    t.string 'text'
+    t.integer 'article_id', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['article_id'], name: 'index_comments_on_article_id'
+  end
+
+  add_foreign_key 'comments', 'articles'
 end

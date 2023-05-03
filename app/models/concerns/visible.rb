902f5a8b51b50b2c5db8cbd635c7cc57ea40ebae
Kazuki Maeda <maedakazuki26@gmail.com>
2023-05-03 23:24:45 +0900

    rubocop

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,14 +1,13 @@
 module Visible
-    extend ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
-    VALID_STATUSES = ['public', 'private', 'archived']
+  VALID_STATUSES = %w[public private archived]
 
-    included do
-        validates :status, inclusion: { in: VALID_STATUSES}
-    end
+  included do
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
 
-
-    def archived?
-        status == 'archived'
-    end
+  def archived?
+    status == 'archived'
+  end
 end
